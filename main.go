d6e90c916283a4f8ad349c2c1850e5b528a3912d
Ethan Kim <ethankim67@gmail.com>
2017-04-11 22:53:02 +0900

    add main.go

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+const version string = "0.0.1"
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Current Version: "+version)
+}
+
+func main() {
+	log.Printf("Listening on port 8000...")
+	http.HandleFunc("/", rootHandler)
+	http.ListenAndServe(":8000", nil)
+}
